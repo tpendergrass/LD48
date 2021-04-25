96bb1c7095e145d95652d5133f202fc506e49f78
Thomas Pendergrass <pendergrassthomas@gmail.com>
2021-04-25 14:17:09 -0600

    Final Level Geometry. Cheat Code for Cave Teleport

---
+++ Assets/Scripts/Cheats.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheats : MonoBehaviour {
+
+    public Transform[] teleports;
+    public GameObject player;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        
+        if(Input.GetKey(KeyCode.RightControl) && Input.GetKeyDown(KeyCode.Keypad0)) {
+            Debug.Log("hi");
+            player.transform.position = teleports[0].position;
+        }
+    }
+}
